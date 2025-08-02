8db6ce174086fb2adf6fe7043723880b16d1d981
devhardy <devhardy@hachanghyeon-ui-MacBookPro.local>
2025-08-02 12:28:04 +0900

    init

---
+++ src/main/java/me/devhardy/locationpharmacy/LocationPharmacyApplication.java
@@ -0,0 +1,13 @@
+package me.devhardy.locationpharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationPharmacyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationPharmacyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/devhardy/locationpharmacy/LocationPharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.devhardy.locationpharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationPharmacyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
